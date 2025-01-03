70915504f977bf981a774efa97b84d993197ab03
seniiya <sasimi1244@gmail.com>
2025-01-03 17:12:36 +0900

    feat: first commit - SpringBoot 프로젝트 빌드

---
+++ pating/src/main/java/umc/pating/PatingApplication.java
@@ -0,0 +1,13 @@
+package umc.pating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatingApplication.class, args);
+	}
+
+}
---
+++ pating/src/test/java/umc/pating/PatingApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.pating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
