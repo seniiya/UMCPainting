ef3d9b9d2b68e1ac8aa8c4b10d19ca4f0a566ed7
qbnyp <moneycloudy@naver.com>
2025-02-07 00:11:31 +0900

    :sparkles: swagger 설정 및 시큐리티 주석처리

---
+++ src/main/java/umc/pating/entity/common/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package umc.pating.entity.common;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        String jwtSchemeName = "jwtAuth";
+
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.APIKEY) // APIKEY 방식으로 설정
+                                .in(SecurityScheme.In.HEADER) // 헤더에서 인증 값 읽기
+                                .description("Enter your JWT token without 'Bearer ' prefix")))
+                .addSecurityItem(new SecurityRequirement().addList(jwtSchemeName))
+                .addServersItem(new Server().url("/").description("https 설정"))
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0.0")
+                        .description("API Documentation without Bearer prefix in Authorization header"));
+    }
+}
